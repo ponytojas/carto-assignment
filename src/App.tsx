0ad8cf77247363ef76a0a0015dd1da8c1877b01f
Daniel Villalobos del Baño <daniel.villalobosdel@icloud.com>
2024-07-26 11:52:46 +0200

    Added toast with sonner

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Flow } from './components/flow/Flow'
 import { Map } from './pages/map/Map'
+import { Toaster } from 'sonner'
 
 function App (): JSX.Element {
   return (
     <>
+      <Toaster richColors position='top-center' />
       <Router>
         <Routes>
           <Route path='/' element={<Flow />} />

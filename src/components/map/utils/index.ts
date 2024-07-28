ca04cc39a969f3731e559d2bb444e3c33db2ed8b
Daniel Villalobos del Baño <daniel.villalobosdel@icloud.com>
2024-07-28 15:26:41 +0200

    Created a default geojson layer properties for deck.gl

---
+++ src/components/map/utils/index.ts
@@ -0,0 +1,14 @@
+export const DEFAULT_GEOJSON_LAYER_PROPS = {
+  pickable: true,
+  stroked: true,
+  filled: true,
+  extruded: false,
+  lineWidthScale: 20,
+  lineWidthMinPixels: 2,
+  getLineColor: [160, 160, 180],
+  getFillColor: [200, 200, 220, 140],
+  getRadius: 100,
+  getPointRadius: 100,
+  getLineWidth: 1,
+  getElevation: 30
+}
